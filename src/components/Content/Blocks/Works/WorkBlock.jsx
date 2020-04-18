2bc973a07437b937856d282d95dedf5aa4ef2ee1
shel1977 <st-shel@yandex.ru>
2020-04-19 00:03:54 +0300

    various fixes

--- src/components/Content/Blocks/Works/WorkBlock.jsx
+++ src/components/Content/Blocks/Works/WorkBlock.jsx
@@ -4,12 +4,14 @@ import styles from './WorkBlock.module.css';
 function WorkBlock(props) {
     return (
         <div className={styles.workBlock}>
-            <div className={styles.headWorkBlock}>
+            <a href='https://github.com/shel1977' target="_blank" rel="noopener noreferrer" >
+                <div className={styles.headWorkBlock}>
                 <img src={props.image} className={styles.imgWorkBlock} alt={props.name}/>
                 <div className={styles.buttonWorkBlock}>
                     <div>show</div>
                 </div>
             </div>
+            </a>
             <div className={styles.footerWorkBlock}>
                 <div className={styles.workTitle}>{props.name}</div>
                 <div className={styles.description}>{props.descriptor}</div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,8 +13,8 @@ function Footer(props) {
                     {props.name}
                 </div>
                 <div className={styles.socialNet}>
-                    <a href="https://www.facebook.com/st.shel" target="_blank" rel="noopener" ><img src={facebook} className={styles.pictSocialNet} alt={'facebook'}/></a>
-                    <a href="https://www.linkedin.com/in/vyacheslav-sheludkov-7975a914b" target="_blank" rel="noopener"><img src={linkedin} className={styles.pictSocialNet} alt={'linkedin'}/></a>
+                    <a href="https://www.facebook.com/st.shel" target="_blank" rel="noopener noreferrer" ><img src={facebook} className={styles.pictSocialNet} alt={'facebook'}/></a>
+                    <a href="https://www.linkedin.com/in/vyacheslav-sheludkov-7975a914b" target="_blank" rel="noopener noreferrer"><img src={linkedin} className={styles.pictSocialNet} alt={'linkedin'}/></a>
                     <a href="mailto:vsheludkov@gmail.com.ru"><img src={mail} className={styles.pictSocialNet} alt={'mail'}/></a>
                     <a href="skype:st_shel?chat"><img src={skype} className={styles.pictSocialNet} alt={'skype'}/></a>
                 </div>
